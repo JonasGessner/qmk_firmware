f45f69982337b78e5781f40a2d0f7f3407c1777f
Jonas Gessner <JonasGessner@users.noreply.github.com>
2021-05-30 23:59:07 +0200

    Move to bootmagic lite because full is deprecated

--- keyboards/pizzakeyboards/pizza65/keymaps/jonas/rules.mk
+++ keyboards/pizzakeyboards/pizza65/keymaps/jonas/rules.mk
@@ -2,10 +2,10 @@
 #   change yes to no to disable
 #
 TAP_DANCE_ENABLE = no 
-BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no	    # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
